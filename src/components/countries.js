ee05b065ccec2ad05e5df49e2b87b4cd7b40f9d9
AbdelhakBLD <abdelhak.belaid@outlook.com>
2020-07-20 09:03:41 +0200

    display all countries responsively with their respective flags

--- src/components/countries.js
+++ src/components/countries.js
@@ -6,17 +6,24 @@ const Countries = ({ countries }) => {
       <center>
         <h1>Countries List</h1>
       </center>
-      {countries.map((countrie) => (
-        <div className="card" key={countrie.numericCode}>
-          <div className="card-body">
-            <h5 className="card-title">{countrie.name}</h5>
-            <h6 className="card-subtitle mb-2 text-muted">
-              {countrie.capital}
-            </h6>
-            <p className="card-text">{countrie.region}</p>
+      <div className="d-flex flex-wrap justify-content-center">
+        {countries.map((countrie) => (
+          <div className="card m-2 flex-item" key={countrie.numericCode}>
+            <img
+              className="card-img-top"
+              src={countrie.flag}
+              alt={"flag of " + countrie.name}
+            ></img>
+            <div className="card-body">
+              <h5 className="card-title">{countrie.name}</h5>
+              <h6 className="card-subtitle mb-2 text-muted">
+                {countrie.capital}
+              </h6>
+              <p className="card-text">{countrie.region}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
