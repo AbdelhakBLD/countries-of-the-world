a3d4baf97a718b702bb74d2d278768a7f76b192b
AbdelhakBLD <abdelhak.belaid@outlook.com>
2020-07-27 02:57:37 +0200

    add scroll to top temporarily

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home.js";
+import ScrollToTop from "react-scroll-to-top";
+import { ReactComponent as MySVG } from "./logo.svg";
 
 class App extends Component {
   render() {
@@ -13,6 +11,7 @@ class App extends Component {
         <div>
           <Switch>
             <Route path="/">
+              <ScrollToTop smooth component={<MySVG />}/>
               <Home />
             </Route>
           </Switch>
